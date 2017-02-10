f83bf6c557e5cb06a482442d5055b750046b53d2
Andre Rampersad <andre.raden+github@gmail.com>
2017-02-10 00:03:52 -0500

    Init Commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const titlw = React.createElement(
+		'h1',
+		{id: 'title', className: 'header'},
+		'Hello World'
+	)
+
+ReactDOM.render(
+	title,
+	document.getElementById('react-container')
+	)
